a419706ac74ad392030eea90a10999e939902c0f
김정학 <wjdgkr3386@gmail.com>
2026-03-01 13:35:15 +0900

    logo 제작및 title 수정

--- backend/src/main/java/com/withyou/backend/common/entity/BaseEntity.java
+++ backend/src/main/java/com/withyou/backend/common/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -17,4 +18,7 @@ public abstract class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
